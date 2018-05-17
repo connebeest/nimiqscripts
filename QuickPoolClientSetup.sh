feec83a01df06b6ccc8a0bfcb1614e99566e7c10
Koenraad Willems <10919552+connebeest@users.noreply.github.com>
2018-05-17 22:33:49 +0200

    Update QuickPoolClientSetup.sh

--- QuickPoolClientSetup.sh
+++ QuickPoolClientSetup.sh
@@ -4,57 +4,58 @@
 killall screen
 
 #Required Setup
-sudo apt-get update
-sudo apt-get -y upgrade
+#sudo apt-get update
+#sudo apt-get -y upgrade
 
 #Install requirements
-sudo apt-get install -y curl git build-essential python
+#sudo apt-get install -y curl git build-essential python
 
 #Setup NodeJS
-curl -sL https://deb.nodesource.com/setup_9.x -o nodesource_setup.sh
-sudo bash nodesource_setup.sh
-sudo apt-get install -y nodejs
+#curl -sL https://deb.nodesource.com/setup_9.x -o nodesource_setup.sh
+#sudo bash nodesource_setup.sh
+#sudo apt-get install -y nodejs
 
-sudo npm install -g gulp
-sudo npm install -g yarn
+#sudo npm install -g gulp
+#sudo npm install -g yarn
 
-git clone https://github.com/nimiq-network/core
+#git clone https://github.com/nimiq-network/core
 
-cd core 
-yarn
+#cd core 
+#yarn
 
-cd ~
-wget https://beeppool.org/linux-ryzen-0.4.1.zip
-unzip linux-ryzen-0.4.1.zip
-cp linux-ryzen/nimiq_node.node core/dist/
-cp linux-ryzen/nimiq_node.node core/build/Release/
-cp linux-ryzen/nimiq_node.node core/build/Release/obj.target/
+#cd ~
+#wget https://beeppool.org/linux-ryzen-0.4.1.zip
+wget https://beeppool.org/linux-haswell-0.4.3.zip
+unzip linux-haswell-0.4.3.zip
+#cp linux-ryzen/nimiq_node.node core/dist/
+#cp linux-ryzen/nimiq_node.node core/build/Release/
+#cp linux-ryzen/nimiq_node.node core/build/Release/obj.target/
 
 #Setting up Mining Runscript
 cd ~
-wget https://raw.githubusercontent.com/connebeest/nimiqscripts/master/mine.sh
+#wget https://raw.githubusercontent.com/connebeest/nimiqscripts/master/mine.sh
 wget https://raw.githubusercontent.com/connebeest/nimiqscripts/master/mine-beeppool.sh
 
 chmod +x mine-beeppool.sh
-chmod +x mine.sh
+#chmod +x mine.sh
 
-scp -r root@srv83003.dus2.fastwebserver.de:/root/core/clients/nodejs/main-full-consensus /root/core/clients/nodejs/
+#scp -r root@srv83003.dus2.fastwebserver.de:/root/core/clients/nodejs/main-full-consensus /root/core/clients/nodejs/
 
 killall screen
 
 #Chosing pool
-echo "1 for Nimiqpool, 2 for Beeppool: "
-read poolchoice
+#echo "1 for Nimiqpool, 2 for Beeppool: "
+#read poolchoice
 
-if [ "$poolchoice" == "1" ]
-then
-echo "You chose Nimiqpool"
-screen -dmS nimiq bash -c "cd ~; ./mine.sh"
-fi
-if [ "$poolchoice" == "2" ]
-then
-echo "You chose Beeppool"
+#if [ "$poolchoice" == "1" ]
+#then
+#echo "You chose Nimiqpool"
+#screen -dmS nimiq bash -c "cd ~; ./mine.sh"
+#fi
+#if [ "$poolchoice" == "2" ]
+#then
+#echo "You chose Beeppool"
 screen -dmS nimiq bash -c "cd ~; ./mine-beeppool.sh"
-fi
+#fi
 
 screen -r
