474663b4c839e35ab1b44964dc4fbeeec4245f99
Koenraad Willems <10919552+connebeest@users.noreply.github.com>
2018-11-03 21:19:43 +0100

    Update mine-beeppool.sh

--- mine-beeppool.sh
+++ mine-beeppool.sh
@@ -2,5 +2,5 @@ hostname="UK-P5000-$RANDOM"
 threads="$(grep processor /proc/cpuinfo | wc -l)"
 echo "Starting miner"
 export UV_THREADPOOL_SIZE="$threads"
-cd ~/beepminer-0.3.4
+cd ~/linux-skylake-avx512
 ./miner --wallet-address="NQ52 2N1A 16S5 1VXG 756D YTPB HFPC TEFU RU75" --miner="$threads" --extra-data="$hostname"
