9bf888ba899fbf1103eebe9adebff84fb0a22f82
Koenraad Willems <10919552+connebeest@users.noreply.github.com>
2020-08-09 21:45:04 +0200

    start_gpu.sh

---
+++ start_gpu.sh
@@ -0,0 +1,6 @@
+clear
+ #!/bin/bash
+ echo "------------------------START Miner----------------------"
+ ./nq-miner/nq-miner -t cuda -a "NQ52 2N1A 16S5 1VXG 756D YTPB HFPC TEFU RU75" -n "$HOSTNAME" -p pool.acemining.co:8443
+ echo "------------------------END Miner----------------------"
+ echo "Something went wrong or you exited"
